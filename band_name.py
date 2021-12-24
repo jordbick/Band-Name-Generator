0b0331f478fcfb7d9226f239770203429f1327d7
Jordan Bickerdyke <jordanbickerdyke@hotmail.co.uk>
2021-12-24 12:54:17 +0000

    1st commit
    Completed project

---
+++ band_name.py
@@ -0,0 +1,11 @@
+#Greeting
+print("Welcome to Band Name Generator.")
+
+#City where the user grew up
+city = input("What city did you grow up in? \n")
+
+#Name of user's first pet
+pet = input("What was the name of your first pet? \n")
+
+#Band name suggestion
+print(f"Your band name could be {city} {pet}")
